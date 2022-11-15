5a4df73b775b030d2111f830c28610d7ffa2c23e
Leyre <113433045+lgarciarob@users.noreply.github.com>
2022-11-15 13:13:16 +0100

    Ejercicio1 (ejemplo Ramón)

---
+++ Ejercicio1.py
@@ -0,0 +1,13 @@
+def saludo(nombre):
+    '''
+    Esta función imprime el mensaje “Hola < nombre >”
+
+    Parámetros:
+    nombre: es el nombre que se imprime
+
+    Salidas:
+    '''
+    print('Hola', nombre)
+    return
+
+saludo('Ramón')
